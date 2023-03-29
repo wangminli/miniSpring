9d9432571253e49822fef92c5beef9e6f27a4a99
wangminli1 <wangminli@jd.com>
2023-03-29 15:28:22 +0800

    课程1，修改：之前的也是课程1

--- mini-spring-core/src/main/java/com/minis/ClassPathXmlApplicationContext.java
+++ mini-spring-core/src/main/java/com/minis/ClassPathXmlApplicationContext.java
@@ -8,17 +8,24 @@ public class ClassPathXmlApplicationContext implements BeanFactory{
     BeanFactory beanFactory;
     //context负责整合容器的启动过程，读外部配置，解析Bean定义，创建BeanFactory
     public ClassPathXmlApplicationContext(String fileName) {
+        //1、获取xml里的bean内容，这时候还没有转成类
         Resource resource = new ClassPathXmlResource(fileName);
+        //2、生成一个bean工厂
         BeanFactory beanFactory = new SimpleBeanFactory();
+        //3、定义一个阅读器，【注意】：这里的bean工厂被赋值到了reader的成员变量里面了，是同一个对象了（从toString就能看出来）
         XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        //4、阅读器阅读resource中的资源，对上面beanFactory对象进行赋值
         reader.loadBeanDefinitions(resource);
+        //5、对自己的beanFactory成员变量赋值
         this.beanFactory = beanFactory;
     }
+    @Override
     //context再对外提供一个getBean，底下就是调用的BeanFactory对应的方法
     public Object getBean(String beanName) throws BeansException {
         return this.beanFactory.getBean(beanName);
     }
+    @Override
     public void registerBeanDefinition(BeanDefinition beanDefinition) {
-        this.beanFactory.registerBeanDefinition(beanDefinition);
+       this.beanFactory.registerBeanDefinition(beanDefinition);
     }
 }
--- mini-spring-core/src/main/java/com/minis/beans/XmlBeanDefinitionReader.java
+++ mini-spring-core/src/main/java/com/minis/beans/XmlBeanDefinitionReader.java
@@ -5,8 +5,9 @@ import com.minis.core.Resource;
 import org.dom4j.Element;
 
 public class XmlBeanDefinitionReader {
-    BeanFactory beanFactory;
+    BeanFactory beanFactory = new SimpleBeanFactory();
     public XmlBeanDefinitionReader(BeanFactory beanFactory) {
+        //wml：这里一定注意， this.beanFactory 和 beanFactory是一个对象，可以打印2个的地址看出来，他们的地址是同一个。
         this.beanFactory = beanFactory;
     }
     public void loadBeanDefinitions(Resource resource) {
